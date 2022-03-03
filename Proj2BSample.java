c5abe765f86a0d189f6d9ac8626c337e34cb3b6c
Marianne Bezaire <marianne.bezaire@andoverma.us>
2022-03-03 10:13:13 -0500

    Further dev

---
+++ Proj2BSample.java
@@ -0,0 +1,18 @@
+public class Proj2BSample
+{
+  public static void main(String[] args)
+  {
+    int x = 3;
+    int y = 2;
+    System.out.println(x);
+    System.out.println(y);
+    x = y;
+    System.out.println(x);
+    System.out.println(y);
+    y = 5;
+    System.out.println(x);
+    System.out.println(y);
+    y = y + 1;
+    System.out.println(y);
+  }
+}
